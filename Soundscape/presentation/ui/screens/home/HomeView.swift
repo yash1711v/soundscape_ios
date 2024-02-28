3ef7f7d6fc98324d914b5096e1af1093bb2bb7ce
FireLord <firelord.xda@gmail.com>
2024-02-28 15:48:41 +0530

    app: Add custom fonts

--- Soundscape/presentation/ui/screens/home/HomeView.swift
+++ Soundscape/presentation/ui/screens/home/HomeView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Home View")
+        NavigationView {
+            ZStack {
+                VStack {
+                    Text("Hi hello")
+                        .font(.custom("WixMadeforText-Regular", size: 20))
+                }
+            }
+        }
     }
 }
 
