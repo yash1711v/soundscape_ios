c8b7d997cc9fd82082c00cb9e41e85af1284c22d
FireLord <firelord.xda@gmail.com>
2024-02-28 11:31:09 +0530

    Initial Commit

---
+++ Soundscape/SoundscapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundscapeApp.swift
+//  Soundscape
+//
+//  Created by admin on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundscapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
