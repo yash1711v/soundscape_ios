4499c68f40ecbd63c61555b3e9ae20beda031e03
FireLord <firelord.xda@gmail.com>
2024-03-27 11:14:31 +0530

    storyDetail: Stretch effect for story images

--- Soundscape/presentation/ui/screens/story/StoryDetailView.swift
+++ Soundscape/presentation/ui/screens/story/StoryDetailView.swift
@@ -16,47 +16,61 @@ struct StoryDetailView: View {
             Color.backgroundGray
             
             ScrollView(.vertical, showsIndicators: false) {
-                Image(mainStorySound.imageName)
-                    .resizable()
-                    .frame(height: 250)
-                
-                Text("About The Story")
-                    .font(.wixMadeFont(.regular, fontSize: .subHeading))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.top)
-                    .padding(.horizontal)
-                    .padding(.bottom, 5)
-                
-                Text(mainStorySound.description)
-                    .font(.wixMadeFont(.regular, fontSize: .title))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.horizontal)
-                
-                Text("Explore Episodes")
-                    .font(.wixMadeFont(.regular, fontSize: .subHeading))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding()
-                    .padding(.top, 20)
-                
-                LazyVStack(spacing: 15) {
-                    ForEach(mainStorySound.episodeList.indices, id: \.self) { index in
-                        let episode = mainStorySound.episodeList[index]
-                        EpisodeListView(episode: episode)
-                            .onTapGesture {
-                                viewModel.episodeList = mainStorySound.episodeList
-                                withAnimation(.spring) {
-                                    viewModel.episode = episode
-                                    viewModel.showBottomPlayer = true
-                                    viewModel.isShuffle = false
-                                    viewModel.playSound(sound: viewModel.episode.songPath)
-                                    viewModel.musicPlayerTitle = "Story Time"
+                Section {
+                    Text("About The Story")
+                        .font(.wixMadeFont(.regular, fontSize: .subHeading))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.top)
+                        .padding(.horizontal)
+                        .padding(.bottom, 5)
+                    
+                    Text(mainStorySound.description)
+                        .font(.wixMadeFont(.regular, fontSize: .title))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal)
+                    
+                    Text("Explore Episodes")
+                        .font(.wixMadeFont(.regular, fontSize: .subHeading))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
+                        .padding(.top, 20)
+                    
+                    LazyVStack(spacing: 15) {
+                        ForEach(mainStorySound.episodeList.indices, id: \.self) { index in
+                            let episode = mainStorySound.episodeList[index]
+                            EpisodeListView(episode: episode)
+                                .onTapGesture {
+                                    viewModel.episodeList = mainStorySound.episodeList
+                                    withAnimation(.spring) {
+                                        viewModel.episode = episode
+                                        viewModel.showBottomPlayer = true
+                                        viewModel.isShuffle = false
+                                        viewModel.playSound(sound: viewModel.episode.songPath)
+                                        viewModel.musicPlayerTitle = "Story Time"
+                                    }
+                                    // Set currentIndex to the index of the selected episode
+                                    viewModel.currentIndex = index
                                 }
-                                // Set currentIndex to the index of the selected episode
-                                viewModel.currentIndex = index
+                        }
+                    }
+                    .padding(.bottom, 130)
+                } header: {
+                    GeometryReader { geometry in
+                        let offsetY = geometry.frame(in: .global).minY
+                        let isScrolled = offsetY > 0
+                        Spacer()
+                            .frame(height: isScrolled ? 250 + offsetY : 250)
+                            .background {
+                                Image(mainStorySound.imageName)
+                                    .resizable()
+                                    
+                                    .offset(y: isScrolled ? -offsetY : 0)
+                                    .scaleEffect(isScrolled ? offsetY / 2000 + 1 : 1)
+                                    .opacity(0.5)
                             }
                     }
+                    .frame(height: 250)
                 }
-                .padding(.bottom, 130)
             }
         }
         .navigationTitle(mainStorySound.name)
